818bd8972ed22d211d6a59d1b81d024ba64ed1d4
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-04-10 13:02:25 -0300

    🚧 implements the function which generates the api's documentation

--- src/main/config/app.config.ts
+++ src/main/config/app.config.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
 import setupRoutes from './route.config';
+import setupDocumentation from './api-docs.config';
 
 const app = fastify({ logger: true });
 
+setupDocumentation(app);
 setupRoutes(app);
 
 export default app;
