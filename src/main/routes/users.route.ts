f9669620a961390bf4ad0b97768f0f2191d69556
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-04-10 13:05:44 -0300

    🔧 add a documentation description

--- src/main/routes/users.route.ts
+++ src/main/routes/users.route.ts
@@ -6,16 +6,60 @@ export default (app: FastifyInstance) => {
     instance
       .get(
         '/users',
+        {
+          schema: {
+            description: 'Obtain all users registred.',
+            tags: ['get-all-users'],
+          }
+        },
         UserController.getAll
       )
       .get(
         '/users/:id',
+        {
+          schema: {
+            description: 'Obtain a user registred by the id.',
+            tags: ['get-user-by-id'],
+            params: {
+              id: { type: 'number' }
+            }
+          }
+        },
         UserController.getById
       )
       .post(
         '/users',
+        {
+          schema: {
+            description: 'Register aa new user.',
+            tags: ['user-registration'],
+            summary: 'qwerty',
+            body: {
+              type: 'object',
+              properties: {
+                id: { type: 'number' },
+                username: { type: 'string' },
+                name: { type: 'string' }
+              }
+            }
+          }
+        },
         UserController.create
+      )
+      .delete(
+        '/users/:id',
+        {
+          schema: {
+            description: 'Delete a user',
+            tags: ['delete-user'],
+            summary: 'qwerty',
+            params: {
+              id: { type: 'number' }
+            }
+          }
+        },
+        UserController.delete
       );
     done();
-  }, { prefix: 'v1/api' })
+  }, { prefix: 'v1/api' });
 }
